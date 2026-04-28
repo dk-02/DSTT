d211fc1d74e3d1d16202c56c5412f73dbb478654
Dora <dora.kasik@fer.hr>
2026-04-28 18:23:45 +0200

    Add api interceptor and auto logout on token expire

--- FRONTEND/frontend/src/App.tsx
+++ FRONTEND/frontend/src/App.tsx
@@ -8,8 +8,38 @@ import { Login } from "./pages/Login";
 import Profile from "./pages/Profile";
 import AdminDashboard from "./pages/AdminDashboard";
 import { ProtectedAdminRoute } from "./components/auth/ProtectedAdminRoute";
+import { useAuthStore } from "./store/useAuthStore";
+import { useEffect } from "react";
+import { jwtDecode } from "jwt-decode";
+
+interface MyTokenPayload {
+    sub: string;
+    email: string;
+    roles: string[];
+    exp: number;
+}
 
 function App() {
+    const { token, logout } = useAuthStore();
+
+    useEffect(() => {
+        if (token) {
+            try {
+                const decoded: MyTokenPayload = jwtDecode(token);
+                const currentTime = Date.now() / 1000;
+
+                if (decoded.exp < currentTime) {
+                    logout();
+                    window.location.href = "/user/login";
+                }
+            } catch (error) {
+                console.error(error);
+                logout();
+            }
+        }
+    }, [token, logout]);
+
+    
     return(
         <Router>
             <Routes>
